3a8b0fc9840de106da3e1f17e7c08d0c1b47bfd2
Jônatas Oliveira <jonatasoliveira62@yahoo.com.br>
2021-11-07 12:48:36 -0300

    Cadastrar usuario.

--- 3DAW/Crud/Cadastrar.php
+++ 3DAW/Crud/Cadastrar.php
@@ -5,42 +5,48 @@ include_once './postgres_connection.php'
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Agenda de contatos</title>
+        <title>Alunos</title>
     </head>
     <body>
-    <form action="?act=save" method="POST" name="form1" >
-            <h1>Agenda de contatos</h1>
-            <hr>
-            <input type="hidden" name="id" <?php
-            // Preenche o id no campo id com um valor "value"
-            if (isset($id) && $id != null || $id != "") {
-                echo "value=\"{$id}\"";
+        <h1>Cadastrar</h1>
+        <?php
+            $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+            if (!empty($dados['CadastrarAluno'])) {
+               
+                $empyt_input = false;
+                //Remoção de espaços vazios no final e inicio
+                $dados = array_map('trim', $dados);
+               
+                //Verifica se os campos estão vazios
+                if(in_array("", $dados)){
+                    $empyt_input = true;
+                    echo "<br>Necessário preencher todos os campos. <br>";
+                }
+               
+                //inserção no banco 
+                if(!$empyt_input){
+                    $sql = "INSERT INTO alunos (Matricula, NomeAluno) VALUES (:matricula, :nome)";
+                    $cad_aluno = $dbconn-> prepare($sql);
+
+                    //atribuição dos parâmetros para o insert
+                    $cad_aluno -> bindParam(':matricula', $dados['matricula']);
+                    $cad_aluno -> bindParam(':nome', $dados['nome']);
+                    $cad_aluno->execute();
+                    if($cad_aluno -> rowCount()){
+                        echo "Aluno cadastrado com sucesso  <br>";
+                    }else {
+                        echo "Usuario não cadastrado com sucesso! <br>";
+                    }
+                }
             }
-            ?> />
-            Nome:
-            <input type="text" name="nome" <?php
-            // Preenche o nome no campo nome com um valor "value"
-            if (isset($nome) && $nome != null || $nome != ""){
-                echo "value=\"{$nome}\"";
-            }
-            ?> />
-            E-mail:
-            <input type="text" name="email" <?php
-            // Preenche o email no campo email com um valor "value"
-            if (isset($email) && $email != null || $email != ""){
-                echo "value=\"{$email}\"";
-            }
-            ?> />
-            Celular:
-            <input type="text" name="celular" <?php
-            // Preenche o celular no campo celular com um valor "value"
-            if (isset($celular) && $celular != null || $celular != ""){
-                echo "value=\"{$celular}\"";
-            }
-            ?> />
-           <input type="submit" value="salvar" />
-           <input type="reset" value="Novo" />
-           <hr>
-        </form>
+        ?>
+        <form name="CadastraAluno" method="POST" action="">
+            <label>Matricula: </label>
+            <input type="text" name="matricula" id="matricula" placeholder="Sua matricula"><br><br>
+       
+            <label>Nome: </label>
+            <input type="text" name="nome" id="nome" placeholder="Nome Completo"><br><br>     
+
+            <input type="submit" value="Cadastrar" name="CadastrarAluno"> 
     </body>
 </html>
