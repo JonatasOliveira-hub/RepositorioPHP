1d2ecb6fc45a77d5db7a2287496d3f87122f4948
Jônatas Oliveira <jonatasoliveira62@yahoo.com.br>
2021-11-24 11:07:55 -0300

    inclusão de CSS

--- Livraria4Ads/index.php
+++ Livraria4Ads/index.php
@@ -7,32 +7,37 @@ session_start();
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="CSS/signin.css" rel="stylesheet">
+    
     <title>Livraria Cristo Vive</title>
 </head>
 
-<body>
-    <h1>Bem Vindo a Livraria</h1>
+<body class="text-center">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <div class="container ">
+        <h1 class="h3 mb-3 font-weight-normal">Bem vindo a livraria Cristo Vive</h1>
+        <form class="form-signin" action="login.php" method="POST">
+            <?php
+            if (isset($_SESSION['nao_autenticado'])) :
+            ?>
+                <p>ERRO: Usuário ou senha inválidos.</p>
+            <?php
+            endif;
+            unset($_SESSION['nao_autenticado']);
+            ?>
 
-    <form action="login.php" method="POST">
-        <!--Colocar o CSS  -->
-        <?php
-        if (isset($_SESSION['nao_autenticado'])) :
-        ?>
-            <p>ERRO: Usuário ou senha inválidos.</p>
-        <?php
-        endif;
-        unset($_SESSION['nao_autenticado']);
-        ?>
-        <input name="usuario" name="text" placeholder="Seu usuário">
-        <input name="senha" type="password" placeholder="Sua senha">
-        <button type="submit"> Entrar </button>
+            <input name="usuario" type="text" class="form-control" placeholder="Seu usuário">
 
-    </form>
+            <input name="senha" type="password" class="form-control" placeholder="Sua senha">
 
+
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+            <p class="mt-5 mb-3 text-muted">&copy; 1995-2021</p>
+
+        </form>
+    </div>
 </body>
 
 </html>
