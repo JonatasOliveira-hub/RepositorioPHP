0de9d8219c65810384ccc08e89ef646f7a474a70
JonatasOliveira-hub <60947663+JonatasOliveira-hub@users.noreply.github.com>
2021-12-16 09:28:00 -0300

    Inicio de incluir clientes

---
+++ ProvaAv2/postgres_connection.php
@@ -0,0 +1,9 @@
+<?php 
+try {
+    $dbconn = new PDO("pgsql:host=localhost;port=5432;dbname=postgres;user=postgres;password=postgres");
+    $dbconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dbconn->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES 'utf8'");
+} catch (PDOException $erro) {
+    echo "Erro na conexão:" . $erro->getMessage();
+}
+?>
