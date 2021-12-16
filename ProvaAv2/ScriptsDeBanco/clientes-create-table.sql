d8fa48ca9235512ee34e725944b9d9c543cc1f2e
JonatasOliveira-hub <60947663+JonatasOliveira-hub@users.noreply.github.com>
2021-12-16 09:28:58 -0300

    Create clientes-create-table.sql

---
+++ ProvaAv2/ScriptsDeBanco/clientes-create-table.sql
@@ -0,0 +1,11 @@
+create table Clientes 
+( id serial PRIMARY KEY,
+  nome VARCHAR (150),
+  endereço VARCHAR(50),
+  postalCode VARCHAR(10),
+  pais VARCHAR (15),
+  cpf character varying(14),
+  Passaporte VARCHAR (15) ,
+  email VARCHAR (30),
+  dataNascimento DATE
+);
